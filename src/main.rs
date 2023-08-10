3bf6bb599d8214ef27c1bddb13f7107261b4206c
zenialexandre <zenialexandre123@gmail.com>
2023-08-10 00:45:02 -0300

    Adding icon window icon.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use dioxus::prelude::*;
 use dioxus_desktop::{Config, WindowBuilder};
-use dioxus_desktop::tao::platform::windows::IconExtWindows;
-use dioxus_desktop::tao::window::Icon;
+use image::GenericImageView;
+use dioxus_desktop::tao::window::Icon as TaoIcon;
 
 fn main() {
-    let icon: Option<Icon>;
-
     dioxus_desktop::launch_cfg(
         app,
         Config::default().with_window(WindowBuilder::new()
             .with_resizable(true).with_title("File Explorer Emulator")
             .with_inner_size(dioxus_desktop::wry::application::dpi::LogicalSize::new(800.0, 400.0))
-            //.with_window_icon(Icon::from_path(dioxus_free_icons::IconShape))
+            .with_window_icon(load_icon_by_path("src/images/icon/simple_face_icon.png"))
             .with_theme(Option::from(dioxus_desktop::tao::window::Theme::Dark))
         )
     );
@@ -21,4 +19,14 @@ fn app(cx: Scope) -> Element {
     cx.render(rsx! (
        div { "Hello, World!" }
     ))
+}
+
+fn load_icon_by_path(file_path: &str) -> Option<TaoIcon> {
+     return if let Ok(image) = image::open(file_path) {
+        let (width, height) = image.dimensions();
+        let rgba_data = image.to_rgba8().into_raw();
+        Some(TaoIcon::from_rgba(rgba_data, width, height).expect("Failed to load icon."))
+    } else {
+        None
+    }
 }
