01030515e75469da94f86aa37cab3405f7d749b2
zenialexandre <zenialexandre123@gmail.com>
2023-10-04 21:14:12 -0300

    search -> double click handler working (open file location)

--- src/search_operation.rs
+++ src/search_operation.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use std::string::ToString;
 use dioxus::html::input_data::keyboard_types::Code;
 use dioxus::prelude::*;
+use dioxus_desktop::{Config, LogicalSize, WindowBuilder};
+use dioxus_desktop::tao::platform::windows::WindowBuilderExtWindows;
 //use regex::Regex;
 use walkdir::WalkDir;
 
@@ -48,13 +50,14 @@ pub(crate) fn create_search_input_field<'a>(cx: &'a ScopeState, files: &'a UseRe
 }
 
 fn execute_search_operation(cx: &ScopeState, files: &UseRef<Files>, search_value: String) {
+    let search_results_map = use_ref(cx,  || HashMap::new());
+
     if search_value.is_empty() {
         files.write().path_stack.clear();
         files.write().path_names.clear();
         files.write().path_stack.push("C://".to_string());
         files.write().reload_path_list();
     } else {
-        let search_results_map = use_ref(cx,  || HashMap::new());
         search_results_map.write().clear();
         search(files.clone(), search_results_map.clone(), search_value.clone());
         create_search_popup(cx, files.clone(), search_results_map.clone());
@@ -62,8 +65,6 @@ fn execute_search_operation(cx: &ScopeState, files: &UseRef<Files>, search_value
 }
 
 fn search(files: UseRef<Files>, search_results_map: UseRef<HashMap<usize, String>>, search_value: String) {
-    //let search_regex_pattern = search_value_props.chars().map(|char| format!(".*{}.*", char)).collect::<String>();
-    //let search_regex = Regex::new(search_value_props.as_str()).unwrap();
     let mut iteration_counter = 1;
 
     for path_name in files.read().path_names.iter() {
@@ -80,12 +81,18 @@ fn search(files: UseRef<Files>, search_results_map: UseRef<HashMap<usize, String
 
 fn create_search_popup(cx: &ScopeState, files: UseRef<Files>, search_results_map: UseRef<HashMap<usize, String>>) {
     let search_results_dom: VirtualDom = VirtualDom::new_with_props(search_results_popup,
-                                                                    search_results_popupProps { files_props: files.clone(), search_result_map_props: search_results_map.clone() });
-    window_helper::create_new_dom_generic_window_state(cx, search_results_dom, "Search");
+        search_results_popupProps { files_props: files, search_results_map_props: search_results_map.clone() });
+    dioxus_desktop::use_window(cx).new_window(search_results_dom, Config::default()
+        .with_window(WindowBuilder::new()
+            .with_resizable(true).with_focused(true)
+            .with_closable(false).with_drag_and_drop(false).with_skip_taskbar(false)
+            .with_window_icon(window_helper::load_icon_by_path("src/images/icon/cool_circle.png"))
+            .with_title("Search").with_inner_size(LogicalSize::new(680.0, 400.0)))
+    );
 }
 
 #[inline_props]
-pub(crate) fn search_results_popup(cx: Scope, files_props: UseRef<Files>, search_result_map_props: UseRef<HashMap<usize, String>>) -> Element {
+pub(crate) fn search_results_popup(cx: Scope, files_props: UseRef<Files>, search_results_map_props: UseRef<HashMap<usize, String>>) -> Element {
     cx.render(rsx!(
         div {
             link { href: "https://fonts.googleapis.com/icon?family=Material+Icons", rel: "stylesheet", }
@@ -99,27 +106,48 @@ pub(crate) fn search_results_popup(cx: Scope, files_props: UseRef<Files>, search
             },
             div {
                 main {
-                    table {
-                        tbody {
-                            search_result_map_props.read().iter().map(|searched_object| {
-                                let path_borrowed = searched_object.1.to_string();
-
-                                rsx!(
-                                    tr {
-                                        class: "folder",
-                                        tabindex: "0",
-                                        ondblclick: move |_| {
-                                            println!("{}", path_borrowed);
-                                        },
-                                        td { h1 { "{path_borrowed}" } }
-                                    }
-                                )
-                            })
-                        }
-                    }
+                    create_search_results_table(cx.scope, files_props, search_results_map_props.clone())
                 }
             }
         }
     ))
 }
 
+fn create_search_results_table<'a>(cx: &'a ScopeState, files_props: &'a UseRef<Files>, search_results_map_props: UseRef<HashMap<usize, String>>) -> LazyNodes<'a, 'a> {
+    if search_results_map_props.read().is_empty() {
+        rsx!(
+            i { class: "material-symbols-outlined", {}, "sentiment_dissatisfied" }
+            h1 { "Nothing found." }
+        )
+    } else {
+        rsx!(
+            search_results_map_props.read().iter().map(|searched_object| {
+                let searched_object_path = searched_object.1.to_string();
+
+                rsx!(
+                    table {
+                        tbody {
+                            tr {
+                                class: "folder",
+                                tabindex: "0",
+                                ondblclick: move |_| {
+                                    handle_double_click_search_event(cx, files_props, searched_object_path.clone());
+                                },
+                                td { h1 { "{searched_object_path}" } }
+                            }
+                        }
+                    }
+                )
+            })
+        )
+    }
+}
+
+fn handle_double_click_search_event(cx: &ScopeState, files_props: &UseRef<Files>, searched_object_path: String) {
+    let mut entry_stack: Vec<&str> = searched_object_path.split("\\").collect();
+    entry_stack.pop();
+    let entry_stack_joined = entry_stack.join("\\");
+    files_props.write().path_stack.push(entry_stack_joined);
+    files_props.write().reload_path_list();
+    dioxus_desktop::use_window(cx).close();
+}
